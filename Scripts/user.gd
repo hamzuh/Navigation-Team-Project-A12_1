e31c537130d38d0c9c31590c306981cdb3f669d4
Hamza Latif <hamzalatif@Mac.lan>
2026-02-20 16:10:05 +0000

    Stopped the agent from moving when the goal is unreachable, and prints the distance for each journey

--- Scripts/user.gd
+++ Scripts/user.gd
@@ -1,16 +1,17 @@
 extends CharacterBody2D
 
-var movement_speed: float = 200.0
-
 @onready var sprite = $Sprite2D
 @onready var movement_target_position: Vector2 = get_parent().get_destination().position
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
+var movement_speed: float = 200.0
 
 func _ready():
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
 	navigation_agent.path_desired_distance = 4.0
 	navigation_agent.target_desired_distance = 4.0
+	navigation_agent.debug_enabled = true
 
 	# Make sure to not await during _ready.
 	actor_setup.call_deferred()
@@ -24,8 +25,20 @@ func actor_setup():
 
 func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
+	if not navigation_agent.is_target_reachable():
+		navigation_agent.debug_enabled = false
+		print("Path not reachable")
+		return
+	navigation_agent.debug_enabled = true
+	print(navigation_agent.distance_to_target())
+	# Use this with scale to print out distance in metres?
+	# get_path_length() / is_target_reachable() might be useful
 
 func _physics_process(delta):
+	if not navigation_agent.is_target_reachable():
+		navigation_agent.debug_enabled = false
+		return
+		
 	if navigation_agent.is_navigation_finished():
 		return
 
