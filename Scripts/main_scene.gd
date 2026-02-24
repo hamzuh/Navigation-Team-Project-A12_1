d0df8a49e118acf15f0d7e20b124df7cfdf4f1f5
Hamza Latif <hamzalatif@Hamzas-MacBook-Air.local>
2026-02-24 12:06:00 +0000

    Added controls for swapping camera view. Press space to switch to full map view.

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
+@onready var follow_cam = $user/playerfollowcam
+@onready var open_cam = $opencam
+
+func _ready():
+	follow_cam.make_current()
+
 func get_destination():
 	return get_node("goal")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("camera_swap"):
+		if follow_cam.is_current():
+			open_cam.make_current()
+		else:
+			follow_cam.make_current()
